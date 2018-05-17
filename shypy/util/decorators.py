732427e947f195e107d1c523732a528d979c8678
grothesk <malte.groth@gotthardt.com>
2018-05-17 10:23:26 +0200

    CheckRaiseDecorator.__call__: replace passing a list with passing packed arguments

--- shypy/util/decorators.py
+++ shypy/util/decorators.py
@@ -9,21 +9,21 @@ class CheckRaiseDecorator():
     the associated condition is not met.
 
     A condition is a Boolean function, an exception inherits from Exception.
-    The arguments are selected by their position. This is done by passing a
-    corresponding list containing indices to __call__.
+    The arguments are selected by their position. This is done by passing
+    indices to __call__.
     """
     def __init__(self, condition, exception):
         self.condition = condition
         self.exception = exception
 
-    def __call__(self, n_args):
+    def __call__(self, *call_args):
         def check_decorator(func):
             @wraps(func)
             def new_func(*args, **kwargs):
-                for n in n_args:
+                for n in call_args:
                     if not self.condition(args[n]):
-                        raise self.exception('arg number {} of function \'{}\' '
-                                             'did not met condition.'.
+                        raise self.exception('arg number {} of function \'{}\''
+                                             ' did not met condition.'.
                                              format(n, func.__name__))
                 return func(*args, **kwargs)
             return new_func
