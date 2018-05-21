2b2834a1074161cf0195744a41e4624dc1948414
grothesk <malte.groth@gotthardt.com>
2018-05-21 16:24:50 +0200

    Extend decorators by log_execution_time decorator.

--- shypy/decorators.py
+++ shypy/decorators.py
@@ -1,4 +1,5 @@
 from functools import wraps
+import time
 
 
 class CheckRaiseDecorator:
@@ -19,12 +20,41 @@ class CheckRaiseDecorator:
     def __call__(self, *call_args):
         def check_decorator(func):
             @wraps(func)
-            def new_func(*args, **kwargs):
+            def wrapper(*args, **kwargs):
                 for n in call_args:
                     if not self.condition(args[n]):
                         raise self.exception('arg number {} of function \'{}\''
                                              ' did not met condition.'.
                                              format(n, func.__name__))
                 return func(*args, **kwargs)
-            return new_func
+            return wrapper
         return check_decorator
+
+
+def log_execution_time(log_func):
+    """
+    Returns a decorator for logging execution time of the decorated function.
+    The way of logging the execution time is defined by log_func. log_func is
+    any function that takes the following 3 arguments:
+    - func_name
+    - start_time
+    - end_time.
+
+    Various log mechanisms are conceivable, e.g.:
+    - writing the execution time to a csv
+    - writing the execution time to a database
+    - ...
+
+    Among other things, the decorator is suitable for observing the processing
+    of growing data sets.
+    """
+    def _log_execution_time(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            log_func(func.__name__, start_time, end_time)
+            return result
+        return wrapper
+    return _log_execution_time
