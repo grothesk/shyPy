03d9b2dea32b7960995d63e029dd9adfece7e45f
grothesk <malte.groth@gmx.net>
2018-12-28 14:39:18 +0100

    Update decorators.py

--- shypy/decorators.py
+++ shypy/decorators.py
@@ -11,9 +11,10 @@ class RepititionsExcluder:
     def exclude_repititions(self, func):
         """
         Excludes the repeated execution of the decorated function.
-        If the decorated function has not yet been registered, it is executed and
-        then registered.
-        If the decorated function already has been registered, it is not executed.
+        If the decorated function has not yet been registered, it is executed
+        and then registered.
+        If the decorated function already has been registered, it is not
+        executed.
         """
         def wrapper(*args, **kwargs):
             if not self.exec_registered(func, *args, **kwargs):
@@ -25,19 +26,20 @@ class RepititionsExcluder:
 
     def exec_registered(self, func, *args, **kwargs):
         """
-        Checks whether a certain function has already been executed with certain
+        Checks whether a certain function has already been executed with
+        certain
         arguments for a certain version number.
         """
         registered = False
 
         if os.path.exists(self.registry_file_path):
-            hash = self.create_hash(func, *args, **kwargs)
+            hash_str = self.create_hash(func, *args, **kwargs)
 
             with open(self.registry_file_path, 'r') as f:
                 lines = f.readlines()
 
                 for line in lines:
-                    if line.replace('\n', '') == hash:
+                    if line.replace('\n', '') == hash_str:
                         registered = True
 
         return registered
@@ -46,41 +48,41 @@ class RepititionsExcluder:
         """
         Creates a hash and writes it to the registry file.
         """
-        hash = self.create_hash(func, *args, **kwargs)
-        self.append_hash_to_registry(hash)
-
+        hash_str = self.create_hash(func, *args, **kwargs)
+        self.append_hash_to_registry(hash_str)
 
     def create_hash(self, func, *args, **kwargs):
         """
-        Creates a hash from a function's name, args, kwargs and given parameters.
-        It is required that str() can be applied to the args, kwargs and parameters.
+        Creates a hash from a function's name, args, kwargs and given
+        parameters.
+        It is required that str() can be applied to the args, kwargs and
+        parameters.
         """
-        hash = hashlib.sha256()
+        hash_str = hashlib.sha256()
 
-        hash.update(func.__name__.encode('utf-8'))
+        hash_str.update(func.__name__.encode('utf-8'))
 
         for arg in args:
-            hash.update(str(type(arg)).encode('utf-8'))
-            hash.update(str(arg).encode('utf-8'))
+            hash_str.update(str(type(arg)).encode('utf-8'))
+            hash_str.update(str(arg).encode('utf-8'))
 
         for key in sorted(kwargs):
-            hash.update(str(type(key)).encode('utf-8'))
-            hash.update(str(key).encode('utf-8'))
+            hash_str.update(str(type(key)).encode('utf-8'))
+            hash_str.update(str(key).encode('utf-8'))
 
-            hash.update(str(type(kwargs[key])).encode('utf-8'))
-            hash.update(str(kwargs[key]).encode('utf-8'))
+            hash_str.update(str(type(kwargs[key])).encode('utf-8'))
+            hash_str.update(str(kwargs[key]).encode('utf-8'))
 
         for p in self.additional_parameters:
-            hash.update(str(type(p)).encode('utf-8'))
-            hash.update(str(p).encode('utf-8'))
+            hash_str.update(str(type(p)).encode('utf-8'))
+            hash_str.update(str(p).encode('utf-8'))
 
-        return hash.hexdigest()
-
+        return hash_str.hexdigest()
 
     def append_hash_to_registry(self, hash):
         with open(self.registry_file_path, 'a') as f:
             f.write('{}\n'.format(hash))
-
+            
 
 class CheckRaiser:
     '''
